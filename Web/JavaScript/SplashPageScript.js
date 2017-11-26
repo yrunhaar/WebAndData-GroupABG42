f91e648029caf627c46e70321227db0390523e28
YOHAN-HP\Yohan Runhaar <spaceyohan@hotmail.com>
2017-11-26 16:33:10 +0100

    Edot + signup page

---
+++ Web/JavaScript/SplashPageScript.js
@@ -0,0 +1,15 @@
+function signin(){
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+
+	if (true) {
+
+	}
+	else{
+			alert("Email/Password combination doesn't exist.");
+	}
+}
+
+function signup(){
+	window.location.href = "SignUp.html";
+}
